ebb62f9c63b7e9420fe57250c282a2a0be543a1b
Alek Karp <aleksander.karp2000@gmail.com>
2026-06-27 19:22:07 -0700

    feat(layout): add root theme provider

---
+++ components/theme-provider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type { ThemeProviderProps } from "next-themes/dist/types";
+import type { ReactNode } from "react";
+
+export function ThemeProvider({
+  children,
+  ...props
+}: ThemeProviderProps & {
+  children: ReactNode;
+}) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
